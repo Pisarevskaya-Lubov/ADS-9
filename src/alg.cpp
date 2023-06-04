f12de417d2030e8b28d65383e78a481189c87eed
Pisarevskaya-Lubov <125238314+Pisarevskaya-Lubov@users.noreply.github.com>
2023-06-04 23:30:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,8 +3,27 @@
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
+#include <string>
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  std::ifstream file(filename);
+  if (!file.is_open()) {
+    throw std::string("ERROR!");
+  }
+  std::string word = "";
+  BST<std::string> bst;
+  while (!file.eof()) {
+    word = "";
+    while (1) {
+      char symb = file.get();
+      if ((symb >= 65 && symb <= 90) || (symb >= 97 && symb <= 122)) {
+        word += tolower(symb);
+      } else {
+        break;
+      }
+    }
+    bst.AddValue(word);
+  }
+  return bst;
 }
